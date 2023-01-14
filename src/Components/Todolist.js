ba4659f7bfec25a751d896e201d68a3f1f1e1ac0
Illia <ilyusha.drozd.2016@gmail.com>
2023-01-14 16:47:11 +0300

    added anim succes

--- src/Components/Todolist.js
+++ src/Components/Todolist.js
@@ -1,7 +1,6 @@
 import Todo from './Todo';
-import checkImg from '../IMG/check.ico';
 import style from '../CSS/Todolist.module.css';
-
+import '../CSS/checkAnimation.css';
 const Todolist = (props) => {
 	const { state, deleteTask, setIsCompleted } = props;
 
@@ -17,13 +16,22 @@ const Todolist = (props) => {
 	return !state.data.length ? ( // if list is empty output massage
 		<h1 className={style.voidList}>
 			All tasks is done!
-			<img src={checkImg} className={style.Img} />
+			<div className='SucessContainer'>
+				<div className='w3-modal-icon w3-modal-success animate'>
+					<span className='w3-modal-line w3-modal-tip animateSuccessTip'></span>
+					<span className='w3-modal-line w3-modal-long animateSuccessLong'></span>
+					<div className='w3-modal-placeholder'></div>
+					<div className='w3-modal-fix'></div>
+				</div>
+			</div>
 		</h1>
 	) : (
 		// else show todos
 		<div className={style.TodoList}>
 			{todos}
-			<h1>Completed {state.completedTodo} todo{state.completedTodo > 1 && 's'} </h1>
+			<h1>
+				Completed {state.completedTodo} todo{state.completedTodo > 1 && 's'}{' '}
+			</h1>
 		</div>
 	);
 };
